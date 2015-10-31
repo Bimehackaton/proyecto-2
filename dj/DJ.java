f530cedfea05c4ea4140e44e7417683d5f058988
JavierLete <jglete@eskura21.com>
2015-10-31 08:57:08 +0100

    Correcciones a visualización y recepción de información

--- dj/DJ.java
+++ dj/DJ.java
@@ -1,53 +1,14 @@
-/*
- import java.awt.KeyEventDispatcher;
- import java.awt.KeyboardFocusManager;
- import java.awt.event.KeyEvent;
 
- public class DJ {
-
- private static boolean wPressed = false;
-
- public static boolean isWPressed() {
- synchronized (DJ.class) {
- return wPressed;
- }
- }
-
- public static void main(String[] args) {
- if(DJ.isWPressed())
- System.out.println("PULSADA");
-        
- KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
-
- @Override
- public boolean dispatchKeyEvent(KeyEvent ke) {
- synchronized (DJ.class) {
- switch (ke.getID()) {
- case KeyEvent.KEY_PRESSED:
- if (ke.getKeyCode() == KeyEvent.VK_W) {
- wPressed = true;
- }
- break;
-
- case KeyEvent.KEY_RELEASED:
- if (ke.getKeyCode() == KeyEvent.VK_W) {
- wPressed = false;
- }
- break;
- }
- return false;
- }
- }
- });
- }
- }
- */
-
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
-import java.io.*;
+import java.nio.file.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DJ {
 
@@ -55,7 +16,10 @@ public class DJ {
 
         JTextField textField = new JTextField();
 
-        textField.addKeyListener(new Keychecker());
+        Utils u = new Utils("../servidor/web/dj.txt");
+
+        textField.addKeyListener(new Keychecker(u));
+        textField.addMouseListener(new Mousechecker(u));
 
         JFrame jframe = new JFrame();
 
@@ -64,49 +28,70 @@ public class DJ {
         jframe.setSize(400, 350);
 
         jframe.setVisible(true);
-        
+
         System.out.println("VERSION 2");
 
     }
 }
 
+class Mousechecker extends MouseAdapter {
+
+    Utils u;
+    public Mousechecker(Utils u)
+    {
+        this.u = u;
+    }
+    @Override
+    public void mousePressed(MouseEvent event) {
+        u.guardar("6");
+    }
+}
+
 class Keychecker extends KeyAdapter {
 
+    Utils u;
+    
+    public Keychecker(Utils u)
+    {
+        this.u = u;
+    }
+    
     @Override
     public void keyPressed(KeyEvent event) {
-        FileOutputStream fos = null;
-        PrintWriter pw = null;
-        try
-        {
-            fos = new FileOutputStream("../servidor/web/dj.txt");
-            pw = new PrintWriter(fos,true);
+        switch (event.getKeyCode()) {
+            case KeyEvent.VK_UP:
+                u.guardar("1");
+                break;
+            case KeyEvent.VK_DOWN:
+                u.guardar("2");
+                break;
+            case KeyEvent.VK_LEFT:
+                u.guardar("3");
+                break;
+            case KeyEvent.VK_RIGHT:
+                u.guardar("4");
+                break;
+            case 32:
+                u.guardar("5");
+            default:
+                System.out.println((int) event.getKeyCode());
+        }
+    }
+}
 
-            switch (event.getKeyCode()) {
-                case KeyEvent.VK_UP:
-                    pw.print("1");
-                    break;
-                case KeyEvent.VK_DOWN:
-                    pw.print("2");
-                    break;
-                case KeyEvent.VK_LEFT:
-                    pw.print("3");
-                    break;
-                case KeyEvent.VK_RIGHT:
-                    pw.print("4");
-                    break;
-                case 32:
-                    pw.print("5");
-                default:
-                    System.out.println((int) event.getKeyCode());
-            }
-            
-            pw.flush();
-            pw.close();
-            fos.close();
+class Utils {
+
+    String ruta;
+
+    public void guardar(String dato) {
+        try {
+            Files.write(Paths.get(ruta), dato.getBytes());
+        } catch (IOException ex) {
+            Logger.getLogger(Keychecker.class.getName()).log(Level.SEVERE, null, ex);
         }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
+    }
+
+    public Utils(String ruta) {
+        this.ruta = ruta;
     }
 }
--- servidor/web/index.php
+++ servidor/web/index.php
@@ -2,22 +2,70 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+date_default_timezone_set("Europe/Madrid");
+
 $app = new Silex\Application();
 
 $app['debug'] = true;
 
-$app->get('/api/mola', function () use($app) {
-    return 'mola ';
+$fmola = __DIR__."/mola.txt";
+$fnomola = __DIR__."/nomola.txt";
+$fcuernos = __DIR__."/cuernos.txt";
+$faccion = __DIR__."/dj.txt";
+$findex = __DIR__."/index.html";
+
+$app->get('/api/mola', function () use($fmola) {
+    $mola = file_get_contents($fmola);
+    $mola++;
+    file_put_contents($fmola, $mola);
+    return 'mola '.$mola;
 });
 
 
-$app->get('/api/nomola', function () {
-   return 'no mola ';
+$app->get('/api/nomola', function () use($fnomola) {
+    $nomola = file_get_contents($fnomola);
+    $nomola++;
+    file_put_contents($fnomola, $nomola);
+    return 'NO mola '.$nomola;
 });
 
+$app->get('/api/cuernos', function () use($fcuernos) {
+    $cuernos = file_get_contents($fcuernos);
+    $cuernos++;
+    file_put_contents($fcuernos, $cuernos);
+    return 'Cuernos '.$cuernos;
+});
 
-$app->get('/api/acciondj', function() {
-    $accion = file_get_contents(__DIR__."/dj.txt");
+$app->get('/api/getmola', function () use($fmola) {
+    $mola = file_get_contents($fmola);
+    return $mola;
+});
+
+$app->get('/api/getnomola', function () use($fnomola) {
+    $nomola = file_get_contents($fnomola);
+    return $nomola;
+});
+
+$app->get('/api/getcuernos', function () use($fcuernos) {
+    $cuernos = file_get_contents($fcuernos);
+    return $cuernos;
+});
+
+$app->get('/api/getdj', function() use($fmola, $fnomola, $fcuernos, $faccion) {
+    $mola = file_get_contents($fmola);
+    $nomola = file_get_contents($fnomola);
+    $cuernos = file_get_contents($fcuernos);
+    $accion = file_get_contents($faccion);
+
+    return '{ "mola": '.$mola.', "nomola": '.$nomola.', "cuernos": '.$cuernos.', "accion": '.$accion.' }';
+});
+
+$app->get('/', function () use ($app, $findex) {
+    return $app->sendFile($findex);
+});
+
+$app->get('/api/acciondj', function() use($faccion) {
+    $accion = file_get_contents($faccion);
     return '{ "accion" : '.$accion.' }';
 });
 
